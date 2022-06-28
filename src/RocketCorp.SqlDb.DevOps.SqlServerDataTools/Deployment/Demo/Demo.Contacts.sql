dfa25dfb5b6a7a96402f6457c5228f8255208e6b
Randi Ratnayake <randi.ratnayake@rkeytech.io>
2022-06-29 09:15:39 +1000

    Seed and Demo Data

---
+++ src/RocketCorp.SqlDb.DevOps.SqlServerDataTools/Deployment/Demo/Demo.Contacts.sql
@@ -0,0 +1,23 @@
+﻿SET IDENTITY_INSERT [RocketCorp].[Contacts] ON 
+GO
+
+IF NOT EXISTS (SELECT * FROM [RocketCorp].[Contacts] WHERE [Id] = 1)
+BEGIN
+    INSERT [RocketCorp].[Contacts] ([Id], [FirstName], [LastName], [IsActive], [DepartmentId])
+    VALUES (1, N'John', N'Smith', 1, 3)
+END
+
+IF NOT EXISTS (SELECT * FROM [RocketCorp].[Contacts] WHERE [Id] = 2)
+BEGIN
+    INSERT [RocketCorp].[Contacts] ([Id], [FirstName], [LastName], [IsActive], [DepartmentId])
+    VALUES (2, N'Anne', N'Marian', 1, 2)
+END
+
+IF NOT EXISTS (SELECT * FROM [RocketCorp].[Contacts] WHERE [Id] = 3)
+BEGIN
+    INSERT [RocketCorp].[Contacts] ([Id], [FirstName], [LastName], [IsActive], [DepartmentId])
+    VALUES (3, N'Ash', N'Oliver', 1, 1)
+END
+
+SET IDENTITY_INSERT [RocketCorp].[Contacts] OFF
+GO
---
+++ src/RocketCorp.SqlDb.DevOps.SqlServerDataTools/Deployment/Seed/Seed.Departments.sql
@@ -0,0 +1,22 @@
+﻿SET IDENTITY_INSERT [RocketCorp].[Departments] ON
+
+IF NOT EXISTS (SELECT * FROM [RocketCorp].[Departments] WHERE [Id] = 1)
+BEGIN
+    INSERT [RocketCorp].[Departments] ([Id], [Name])
+    VALUES (1, N'Admin (SSDT)')
+END
+
+IF NOT EXISTS (SELECT * FROM [RocketCorp].[Departments] WHERE [Id] = 2)
+BEGIN
+    INSERT [RocketCorp].[Departments] ([Id], [Name])
+    VALUES (2, N'Finance (SSDT)')
+END
+
+IF NOT EXISTS (SELECT * FROM [RocketCorp].[Departments] WHERE [Id] = 3)
+BEGIN
+    INSERT [RocketCorp].[Departments] ([Id], [Name])
+    VALUES (3, N'Human Resources (SSDT)')
+END
+
+SET IDENTITY_INSERT [RocketCorp].[Departments] OFF
+GO
