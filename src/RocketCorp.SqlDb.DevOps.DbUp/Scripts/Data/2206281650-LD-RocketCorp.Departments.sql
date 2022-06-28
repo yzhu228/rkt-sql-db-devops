450a9837aa41fe1bb3e7a122ab5557ae854eac15
Randi Ratnayake <randi.ratnayake@rkeytech.io>
2022-06-29 09:06:02 +1000

    Seed Db Up

--- src/RocketCorp.SqlDb.DevOps.DbUp/Scripts/Data/2206281650-LD-RocketCorp.Departments.sql
+++ src/RocketCorp.SqlDb.DevOps.DbUp/Scripts/Data/2206281650-LD-RocketCorp.Departments.sql
@@ -1,8 +1,8 @@
 ﻿SET IDENTITY_INSERT [RocketCorp].[Departments] ON 
 
-INSERT [RocketCorp].[Departments] ([Id], [Name]) VALUES (1, N'Admin')
-INSERT [RocketCorp].[Departments] ([Id], [Name]) VALUES (2, N'Finance')
-INSERT [RocketCorp].[Departments] ([Id], [Name]) VALUES (3, N'Human Resources')
+INSERT [RocketCorp].[Departments] ([Id], [Name]) VALUES (1, N'Admin (DbUp)')
+INSERT [RocketCorp].[Departments] ([Id], [Name]) VALUES (2, N'Finance (DbUp)')
+INSERT [RocketCorp].[Departments] ([Id], [Name]) VALUES (3, N'Human Resources (DbUp)')
 
 SET IDENTITY_INSERT [RocketCorp].[Departments] OFF
 GO
